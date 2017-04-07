e06e6b6978b542279be695f0d5955f8622610169
Chino Del Mundo <Chino.DelMundo@maximintegrated.com>
2017-04-07 17:31:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS Music Player
+
+Created as part of Javascript Programming Course in [https://www.vikingcodeschool.com/](https://www.vikingcodeschool.com/).
+
+See demo: [https:/chinodelmundo.github.io/js-music-player](https:/chinodelmundo.github.io/js-music-player)
---
+++ js/script.js
@@ -0,0 +1,109 @@
+var init = function(){
+		$(".play-button").on("click", function(e){
+			$target = $(e.target);
+			
+			if($target.hasClass("fa-play")){
+				changeSelected($target.parent());
+			}else{
+				$target.removeClass("fa-pause")
+					   .addClass("fa-play");
+				
+				$target.parent().removeClass("song-selected");
+				
+				$(".fa-pause-circle-o").removeClass("fa-pause-circle-o")
+					   .addClass("fa-play-circle-o");
+			}
+			
+			togglePlay($target.parent());
+		});
+		
+
+		$(".player-button").on("click", function(e){
+			$target = $(e.target);
+			
+			if($target.hasClass("fa-pause-circle-o")){
+				$target.removeClass("fa-pause-circle-o")
+						.addClass("fa-play-circle-o");
+						
+				$songItem = $(".fa-pause").parent();
+				$(".fa-pause").removeClass("fa-pause")
+						.addClass("fa-play");
+				
+				togglePlay($songItem);
+			}else if($target.hasClass("fa-play-circle-o")){
+				var title = $target.parent().parent().find("b").text();
+			
+				console.log(title);
+			
+				$button = $(".song-item b:contains(" + title + ")").parent().parent().find("button");
+				
+				$target.removeClass("fa-play-circle-o")
+						.addClass("fa-pause-circle-o");
+				
+				$button.removeClass("fa-play")
+						.addClass("fa-pause");
+				
+				$songItem = $button.parent();
+				togglePlay($songItem);
+			}else if($target.hasClass("fa-step-backward")){
+				$currentSongItem = $(".song-selected");
+				
+				if($currentSongItem.prev().length){
+					$prevSongItem = $currentSongItem.prev();
+				}else{
+					$prevSongItem = $(".song-list").children().last();
+				}
+				
+				changeSelected($prevSongItem);
+				togglePlay($currentSongItem);
+				togglePlay($prevSongItem);
+			}else if($target.hasClass("fa-step-forward")){
+				$currentSongItem = $(".song-selected");
+				
+				if($currentSongItem.next().length){
+					$nextSongItem = $currentSongItem.next();
+				}else{
+					$nextSongItem = $(".song-list").children().first();
+				}
+				
+				changeSelected($nextSongItem);
+				togglePlay($currentSongItem);
+				togglePlay($nextSongItem);
+			}
+		});
+	}
+
+var changeSelected = function($selectedSongItem){
+	var title = $selectedSongItem.find("b").text();
+	var artist = $selectedSongItem.find("i").text();
+	
+	$(".play-button").removeClass("fa-pause")
+					 .addClass("fa-play");
+	
+	$button = $selectedSongItem.find("button");
+	$button.removeClass("fa-play")
+		   .addClass("fa-pause");
+	
+	$(".song-selected").removeClass("song-selected")
+	$selectedSongItem.addClass("song-selected");
+	
+	$(".fa-play-circle-o").removeClass("fa-play-circle-o")
+		   .addClass("fa-pause-circle-o");
+	
+	$("#playing-song-title").text(title);
+	$("#playing-song-artist").text(artist);
+}
+
+var togglePlay = function($selectedSongItem){
+	var song = $selectedSongItem.find("audio").first().get(0);
+	
+	if(song.paused){
+		song.play();
+	}else{
+		song.pause();
+	}
+}
+
+$(document).ready( function(){
+	init();
+});
